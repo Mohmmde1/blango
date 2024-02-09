715381eedd9ab8760eb62e443325109d9beb9f6d
Mohammed Alnashriy <alnashriymohammed@gmail.com>
2024-02-09 10:55:25 +0000

    Finish security ad passwords

--- blango/settings.py
+++ blango/settings.py
@@ -99,7 +99,7 @@ class Dev(Configuration):
         },
         {
             'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-        },
+
         {
             'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
         },
@@ -138,27 +138,29 @@ class Dev(Configuration):
     CSRF_COOKIE_SAMESITE = 'None'
     SESSION_COOKIE_SAMESITE = 'None'
     CRISPY_TEMPLATE_PACK = "bootstrap4"
+
     LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "formatters": {
-        "verbose": {
-            "format": "{levelname} {asctime} {module} {process:d} {thread:d} {message}",
-            "style": "{",
-        },
-    },
-    "handlers": {
-        "console": {
-            "class": "logging.StreamHandler",
-            "stream": "ext://sys.stdout",
-            "formatter": "verbose",
-        },
-    },
-    "root": {
-        "handlers": ["console"],
-        "level": "DEBUG",
-    },
-}
+      "version": 1,
+      "disable_existing_loggers": False,
+      "formatters": {
+          "verbose": {
+              "format": "{levelname} {asctime} {module} {process:d} {thread:d} {message}",
+              "style": "{",
+          },
+      },
+      "handlers": {
+          "console": {
+              "class": "logging.StreamHandler",
+              "stream": "ext://sys.stdout",
+              "formatter": "verbose",
+          },
+      },
+      "root": {
+          "handlers": ["console"],
+          "level": "DEBUG",
+      },
+    }
+
 
 class Prod(Dev):
   
