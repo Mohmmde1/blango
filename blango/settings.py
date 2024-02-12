7bc97f789d5a4280d463acc35773b9cc028077e0
Mohammed Alnashriy <alnashriymohammed@gmail.com>
2024-02-12 18:03:24 +0000

    Finish custom User model

--- blango/settings.py
+++ blango/settings.py
@@ -48,7 +48,9 @@ class Dev(Configuration):
         'django.contrib.messages',
         'django.contrib.staticfiles',
         'blog',
+        'blango_auth',
         "debug_toolbar",
+
     ]
 
     MIDDLEWARE = [
@@ -164,6 +166,7 @@ class Dev(Configuration):
       },
     }
     INTERNAL_IPS = ["192.168.10.226"]
+    AUTH_USER_MODEL = "blango_auth.User"
 
 
 class Prod(Dev):
