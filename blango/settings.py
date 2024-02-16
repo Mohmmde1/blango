a7c1e589ee37c880767b1a8bd1bce6cbe1b7ce1c
Mohammed Alnashriy <alnashriymohammed@gmail.com>
2024-02-16 13:01:30 +0000

    Finish django Allauth

--- blango/settings.py
+++ blango/settings.py
@@ -46,11 +46,15 @@ class Dev(Configuration):
         'django.contrib.contenttypes',
         'django.contrib.sessions',
         'django.contrib.messages',
+        'django.contrib.sites',
         'django.contrib.staticfiles',
         'blog',
         'blango_auth',
         "debug_toolbar",
-
+        "allauth", 
+        "allauth.account", 
+        "allauth.socialaccount", 
+        "allauth.socialaccount.providers.google"
     ]
 
     MIDDLEWARE = [
@@ -169,6 +173,11 @@ class Dev(Configuration):
     AUTH_USER_MODEL = "blango_auth.User"
     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
     ACCOUNT_ACTIVATION_DAYS = 7
+    SITE_ID = 1
+    ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+    ACCOUNT_EMAIL_REQUIRED = True
+    ACCOUNT_USERNAME_REQUIRED = False
+    ACCOUNT_AUTHENTICATION_METHOD = "email"
     
 
 
