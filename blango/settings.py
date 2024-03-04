7610b870b95c1cbf96fcea18b78ace51e6fbf9f1
Mohammed Alnashriy <alnashriymohammed@gmail.com>
2024-03-04 07:56:13 +0000

    Finish authentication

--- blango/settings.py
+++ blango/settings.py
@@ -55,7 +55,8 @@ class Dev(Configuration):
         "allauth.account", 
         "allauth.socialaccount", 
         "allauth.socialaccount.providers.google",
-        "rest_framework"
+        "rest_framework",
+        "rest_framework.authtoken"
     ]
 
     MIDDLEWARE = [
@@ -86,6 +87,14 @@ class Dev(Configuration):
             },
         },
     ]
+
+    REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+    ]
+}
 
     WSGI_APPLICATION = 'blango.wsgi.application'
 
